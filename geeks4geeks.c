6582bf6dc1a3cb22e4a9956d5d0b40b4b116fdb1
mli42 <mli@student.42.fr>
2024-08-22 12:34:49 +0200

    clean

--- geeks4geeks.c
+++ geeks4geeks.c
@@ -108,26 +108,24 @@ char* reverse_dns_lookup(char* ip_addr)
 }
 
 // make a ping request
-void send_ping(int ping_sockfd,
+void send_ping(
+  int ping_sockfd,
   struct sockaddr_in* ping_addr,
-  char* ping_dom, char* ping_ip,
-  char* rev_host)
-{
+  const char *const ping_dom,
+  char* ping_ip,
+  char* rev_host
+) {
   unsigned int addr_len;
-  int ttl_val = 64, msg_count = 0, i, flag = 1,
-    msg_received_count = 0;
+  int ttl_val = 64, msg_count = 0, i, msg_received_count = 0;
 
   signal(SIGINT, intHandler); // catching interrupt
-  // receive buffer
-  // ip header is included when receiving
-  // from raw socket
+  // receive buffer ip header is included when receiving from raw socket
   char rbuffer[128];
   struct ping_pkt* r_pckt;
 
   struct ping_pkt pckt;
   struct sockaddr_in r_addr;
   struct timespec time_start, time_end, tfs, tfe;
-  long double rtt_msec = 0, total_msec = 0;
   struct timeval tv_out;
   tv_out.tv_sec = RECV_TIMEOUT;
   tv_out.tv_usec = 0;
@@ -136,26 +134,21 @@ void send_ping(int ping_sockfd,
 
   // set socket options at ip to TTL and value to 64,
   // change to what you want by setting ttl_val
-  if (setsockopt(ping_sockfd, SOL_IP, IP_TTL, &
-    ttl_val, sizeof(ttl_val))
-    != 0) {
+  if (setsockopt(ping_sockfd, IPPROTO_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) != 0) {
     printf("\nSetting socket options to TTL failed !\n");
     return;
   }
-
   else {
     printf("\nSocket set to TTL..\n ");
   }
 
   // setting timeout of recv setting
-  setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO,
-    (const char*)&
-    tv_out, sizeof tv_out);
+  setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof tv_out);
 
   // send icmp packet in an infinite loop
   while (pingloop) {
     // flag is whether packet was sent or not
-    flag = 1;
+    int flag = 1;
 
     // filling packet
     bzero(&pckt, sizeof(pckt));
@@ -186,12 +179,11 @@ void send_ping(int ping_sockfd,
     if (recvfrom(ping_sockfd, rbuffer, sizeof(rbuffer), 0, (struct sockaddr*)&r_addr, &addr_len) <= 0 && msg_count > 1) {
       printf("\nPacket receive failed !\n");
     }
-
     else {
       clock_gettime(CLOCK_MONOTONIC, &time_end);
 
       double timeElapsed = ((double)(time_end.tv_nsec - time_start.tv_nsec)) / 1000000.0;
-      rtt_msec = (time_end.tv_sec - time_start.tv_sec) * 1000.0 + timeElapsed;
+      long double rtt_msec = (time_end.tv_sec - time_start.tv_sec) * 1000.0 + timeElapsed;
 
       // if packet was not sent, don't receive
       if (flag) {
@@ -207,11 +199,8 @@ void send_ping(int ping_sockfd,
     }
   }
   clock_gettime(CLOCK_MONOTONIC, &tfe);
-  double timeElapsed
-    = ((double)(tfe.tv_nsec - tfs.tv_nsec)) / 1000000.0;
-
-  total_msec = (tfe.tv_sec - tfs.tv_sec) * 1000.0
-    + timeElapsed;
+  double timeElapsed = ((double)(tfe.tv_nsec - tfs.tv_nsec)) / 1000000.0;
+  long double total_msec = (tfe.tv_sec - tfs.tv_sec) * 1000.0 + timeElapsed;
 
   printf("\n === %s ping statistics === \n ", ping_ip);
   printf("\n %d packets sent, %d packets received, %f percent packet loss.Total time:%Lf ms.\n\n ", msg_count, msg_received_count, ((msg_count - msg_received_count) / msg_count) * 100.0, total_msec);
@@ -220,30 +209,24 @@ void send_ping(int ping_sockfd,
 // Driver Code
 int main(int argc, char* argv[])
 {
-  int sockfd;
-  char *ip_addr, *reverse_hostname;
   struct sockaddr_in addr_con;
-  int addrlen = sizeof(addr_con);
-  char net_buf[NI_MAXHOST];
 
   if (argc != 2) {
     printf("\nFormat %s <address> \n ", argv[0]);
     return 0;
   }
 
-  ip_addr = dns_lookup(argv[1], &addr_con);
+  char *ip_addr = dns_lookup(argv[1], &addr_con);
   if (ip_addr == NULL) {
-    printf(
-      "\nDNS lookup failed !Could not resolve hostname !\n ");
+    printf("\nDNS lookup failed !Could not resolve hostname !\n ");
     return 0;
   }
 
-  reverse_hostname = reverse_dns_lookup(ip_addr);
+  const char *reverse_hostname = reverse_dns_lookup(ip_addr);
   printf("\nTrying to connect to '%s' IP: %s\n ", argv[1], ip_addr);
   printf("\nReverse Lookup domain: %s ", reverse_hostname);
 
-  // socket()
-  sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+  const int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
   if (sockfd < 0) {
     printf("\nSocket file descriptor not received !!\n");
     return 0;
